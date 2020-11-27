a37c3d3cfe999d8bfa63942ebb875c64cebfeca5
spenser6131 <brinkman.spenser@gmail.com>
2020-11-27 10:31:48 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, except: [:index, :delete]
+  resources :school_classes, except: [:index, :delete]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20201127174350) do
+
+  create_table "school_classes", force: :cascade do |t|
+    t.string  "title"
+    t.integer "room_number"
+  end
 
   create_table "students", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
